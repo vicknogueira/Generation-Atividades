5dfe80bcf85fee4970b35fbf73104d0cf814a0b8
Vick <vicknogueira199@gmail.com>
2020-03-02 15:05:53 -0300

    Exercícios de função

--- Atividade/Funcoes/ExercisesFuncoes/assets/js/substring.js
+++ Atividade/Funcoes/ExercisesFuncoes/assets/js/substring.js
@@ -61,20 +61,33 @@
 
 // var digite = prompt("Digite algo: ");
 // 6 --------------------------------------------------------
-var texto = "olaaa tudo bem"
-var vetor = texto.split(' ') // transformei uma string em um vetor
-var resultado = ''
-for(var i = 0; i < vetor.length; i++){
-    resultado += " " + [vetor[i]]
-    if(vetor[i].length > vetor[i].length[0]){
-        document.write("A maior palavra é " + vetor[i])
+// var texto = prompt("Digite uma frase: ");
+// var vetor = texto.split(" ");
+// var tamanho = vetor[0].length;
+// var titulo = document.getElementById('resultado')
+// var palavra = '';
+// var resultado = document.getElementById('digitado')
+// for(var i = 1; i < vetor.length; i++){
+//     if(tamanho < vetor[i].length){
+//         tamanho = vetor[i].length;
+//         vetor[i] = vetor[i].replace(vetor[i].slice(0, 1), vetor[i].slice(0, 1).toUpperCase())
+//         palavra = vetor[i]
+//     }
+// }
+// titulo.innerHTML = 'Exercício 6: Pegar uma frase e detectar qual a maior palavra:'
+// resultado.innerHTML = `<br><br>A palavra que você digitou foi "${palavra}" e seu tamanho é ${tamanho}`;
+// 7 ---------------------------------------------------------
+var texto = prompt("Digite uma frase: ");
+var vetor = texto.split("");
+var letras = vetor[0]
+var titulo = document.getElementById('resultado')
+var palavra = '';
+var resultado = document.getElementById('digitado')
+var vogal = ['a','e','i','o','u'];
+var recebe = vogal[0]
+alert(vetor);
+for(var i = 1; i < vetor.length; i++){
+    if(letras == recebe[i]){
+        letras = recebe[i];
     }
-    if(vetor[i].length > vetor[i, 1].length){
-        document.write("A maior palavra é " + vetor[i])
-    }
-    if(vetor[i].length > vetor[i, 2].length){
-        document.write("A maior palavra é " + vetor[i])
-    }
-    alert(resultado)
-    // alert(vetor[i].length)
 }
